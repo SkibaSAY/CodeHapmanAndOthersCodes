25cc7f6fbf44bfe6471f22fe238afbd898b31138
Anton Slastuhin <antonslastuhin@gmail.com>
2022-04-25 20:42:11 +0300

    --------------------------------

--- Wormochka/MainWindow.xaml.cs
+++ Wormochka/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HaphmanCodeLibrary;
 
 namespace Wormochka
 {
@@ -23,6 +26,22 @@ namespace Wormochka
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void RunHaphman_Click(object sender, RoutedEventArgs e)
+        {
+            var sd = new OpenFileDialog();
+            if (sd.ShowDialog() == false) return;
+            var text = File.ReadAllText(sd.FileName);
+            inputText.Text = text;
+            var dict = HaphmanCode.BuildCode(sd.FileName);
+
+            codesText.Text = String.Join("\n",dict.ToArray());
+
+            var codedText = HaphmanCode.Coding(text, dict);
+            encryptText.Text = codedText;
+            var decodText = HaphmanCode.Decoding(codedText, dict);
+            decryptText.Text = decodText;
         }
     }
 }
