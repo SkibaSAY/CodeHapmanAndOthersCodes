93477a2014909f3f8bb21d2aee0bf2a210096434
pr0gramm-ist <pr0gramm-ist@yandex.ru>
2023-04-21 21:26:55 +0300

    Нужно сделать, чтобы принять изменения

--- CodeHaphmanLibrary/Codes/ArifmeticalCode.cs
+++ CodeHaphmanLibrary/Codes/ArifmeticalCode.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace CodesLibrary
 {
@@ -15,11 +16,15 @@ namespace CodesLibrary
         public void Code(string inputFilePath, string outputFilePath, string resoursesPath)
         {
             var inputText = File.ReadAllText(inputFilePath);
+            var result = Coding(inputText);
+            File.WriteAllText(outputFilePath, result);
+
+            var jsonResourse = JsonConvert.SerializeObject(alphabetOfProbabilities);
+            File.WriteAllText(resoursesPath, jsonResourse);
         }
 
         public string Coding(string inputText)
         {
-            //тут пока наивный алгоритм. Без оптимизации под длинный текст
             //заполняем алфавит
             foreach (var symbol in inputText)
             {
@@ -61,7 +66,7 @@ namespace CodesLibrary
                 int i = 0;
                 while(stringCodeStart.Length > i && stringCodeEnd.Length > i && stringCodeStart[i] == stringCodeEnd[i])
                 {
-                    if (stringCodeStart[i] != ',')
+                    if (stringCodeStart[i] != ',' || stringCodeStart[i] == ',' && !res.Contains(","))
                     {
                         res += stringCodeStart[i];
                     }
@@ -84,7 +89,67 @@ namespace CodesLibrary
 
         public void Decode(string inputFilePath, string outputFilePath, string resoursesPath)
         {
-            throw new NotImplementedException();
+            var inputNumber = File.ReadAllText(inputFilePath);
+
+            var jsonResourse = File.ReadAllText(resoursesPath);
+
+            alphabetOfProbabilities = JsonConvert.DeserializeObject<SortedList<char, uint>>(jsonResourse);
+
+            var decodedText = Decoding(inputNumber);
+            File.WriteAllText(outputFilePath, decodedText);
+        }
+
+        public string Decoding(string inputNumber)
+        {
+            ulong countOfLetters = 0;
+            var codeNumber = new StringBuilder(inputNumber);
+
+            //заполняем алфавит
+            foreach (var symbol in alphabetOfProbabilities)
+            {
+                countOfLetters += symbol.Value;
+            }
+
+            double currentCodeStart = 0;
+            double currentCodeEnd = countOfLetters;
+            string stringCodeStart = currentCodeStart.ToString();
+            string stringCodeEnd = currentCodeEnd.ToString();
+            var res = "";
+
+            for (uint i = 0; i < countOfLetters;i++)
+            {
+                var curNumber = uint.Parse(codeNumber.ToString());
+                var lengthOfInterval = currentCodeEnd - currentCodeStart;
+                var startOfInterval = currentCodeStart;
+                currentCodeStart = 0;
+                //foreach (var letter in alphabetOfProbabilities)
+                //{
+                //    if (letter.Key == symbol) break;
+                //    currentCodeStart += letter.Value;
+                //}
+
+                //currentCodeStart *= lengthOfInterval / countOfLetters;
+                //currentCodeStart += startOfInterval;
+                //currentCodeEnd = currentCodeStart + alphabetOfProbabilities[symbol] * lengthOfInterval / countOfLetters;
+
+                //stringCodeStart = currentCodeStart.ToString();
+                //stringCodeEnd = currentCodeEnd.ToString();
+                //int i = 0;
+                //while (stringCodeStart.Length > i && stringCodeEnd.Length > i && stringCodeStart[i] == stringCodeEnd[i])
+                //{
+                //    if (stringCodeStart[i] != ',')
+                //    {
+                //        res += stringCodeStart[i];
+                //    }
+                //    stringCodeStart = stringCodeStart.Remove(0, 1);
+                //    stringCodeEnd = stringCodeEnd.Remove(0, 1);
+                //}
+                //currentCodeStart = double.Parse(stringCodeStart);
+                //currentCodeEnd = double.Parse(stringCodeEnd);
+            }
+
+            return res + stringCodeEnd[0];
+
         }
     }
 }
