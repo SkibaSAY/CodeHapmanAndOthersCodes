32c8a50a2f4f03327be81046c8a53a92a4811a77
pr0gramm-ist <pr0gramm-ist@yandex.ru>
2023-04-24 21:41:56 +0300

    Заготовка Шеннона

--- CodeHaphmanLibrary/Codes/PhanoShenonCode.cs
+++ CodeHaphmanLibrary/Codes/PhanoShenonCode.cs
@@ -1,14 +1,40 @@
 ﻿using CodeLibrary;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CodesLibrary
 {
-    public class PhanoShenonCode
+    public class PhanoShenonCode : ICode
     {
+        public void Code(string inputText, out string outputText, out string resourses)
+        {
+            throw new NotImplementedException();
+        }
 
+        public string Coding(string inputText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double CompressionRate(string inputText, string outputText, string resourses)
+        {
+            var beforeCodingSize = inputText.Length;
+            var afterCodingSize = outputText.Length + resourses.Length;
+            return beforeCodingSize / afterCodingSize;
+        }
+
+        public void Decode(string inputText, out string outputText, string resourses)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decoding(string outputText, string resourses)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
